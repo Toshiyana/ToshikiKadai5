7a5c863ec6dcb460325833aae72d4dc9e040a7e9
akio0911 <akio0911@gmail.com>
2021-11-08 18:04:15 +0900

    feedback

--- 5/ViewController.swift
+++ 5/ViewController.swift
@@ -12,43 +12,33 @@ class ViewController: UIViewController {
     @IBOutlet private weak var divisorTextField: UITextField!
     @IBOutlet private weak var resultLabel: UILabel!
 
-    enum DivideError {
-        case noDividend
-        case noDivisor
-        case assignZeroToDivisor
+    private enum Message {
+        static let noDividend = "割られる数を入力してください"
+        static let noDivisor = "割る数を入力してください"
+        static let assignZeroToDivisor = "割る数には0を入力しないでください"
     }
 
     @IBAction private func divideButtonPressed(_ sender: UIButton) {
         guard let dividend = Double(dividendTextField.text ?? "") else {
-            showAlert(error: .noDividend)
+            showAlert(message: Message.noDividend)
             return
         }
 
         guard let divisor = Double(divisorTextField.text ?? "") else {
-            showAlert(error: .noDivisor)
+            showAlert(message: Message.noDivisor)
             return
         }
-        if divisor == 0.0 {
-            showAlert(error: .assignZeroToDivisor)
+
+        guard divisor != 0.0 else {
+            showAlert(message: Message.assignZeroToDivisor)
             return
         }
 
         resultLabel.text = String(dividend / divisor)
     }
 
-    private func showAlert(error: DivideError) {
-        let alertText: String
-
-        switch error {
-        case .noDividend:
-            alertText = "割られる数を入力してください"
-        case .noDivisor:
-            alertText = "割る数を入力してください"
-        case .assignZeroToDivisor:
-            alertText = "割る数には0を入力しないでください"
-        }
-
-        let alert = UIAlertController(title: "課題5", message: alertText, preferredStyle: .alert)
+    private func showAlert(message: String) {
+        let alert = UIAlertController(title: "課題5", message: message, preferredStyle: .alert)
         let cancelAction = UIAlertAction(title: "OK", style: .cancel)
         alert.addAction(cancelAction)
         present(alert, animated: true)
